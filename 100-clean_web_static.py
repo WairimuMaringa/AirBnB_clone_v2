e6760b9d7c129370eaabdb607e58f5c7c27a27de
WairimuMaringa <makena96wairimu@gmail.com>
2022-12-22 14:11:46 -0800

    advanced one

---
+++ 100-clean_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Fabric script htat deletes outofdate archives """
+from fabric.api import *
+env.hosts = ['54.160.102.228', '52.91.121.211']
+env.user = 'ubuntu'
+
+
+def do_clean(number=0):
+    """ Delete unnecessary archives. """
+    number = int(number)
+
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
