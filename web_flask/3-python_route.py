cc182f54eeb85df681a4c1e2ba132e6c58d13252
WairimuMaringa <makena96wairimu@gamil.com>
2023-01-15 05:56:26 -0800

    task three

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Script that starts a web flask application.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def path():
+    """
+    Function to display text on screen.
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def path_hbnb():
+    """
+    Function to display txt on screen.
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<custom>', strict_slashes=False)
+def path_c_custom(custom):
+    """
+    Function to display text on screen.
+    """
+    return 'C %s' % custom.replace('_', ' ')
+
+
+@app.route('/python', defaults={'custom': 'is cool'}, strict_slashes=False)
+@app.route('/python/<path:custom>', strict_slashes=False)
+def path_python_custom(custom):
+    """
+    Function to display text on screen.
+    """
+    return "Python {}".format(custom.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
