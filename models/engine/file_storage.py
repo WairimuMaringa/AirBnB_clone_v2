1280a820a8d4aa88a07c13cc2d5f76fee07cb415
WairimuMaringa <makena96wairimu@gmail.com>
2023-01-18 10:59:36 -0800

    edit to file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,14 @@ class FileStorage:
 
     def all(self):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls:
+            new_objects = {}
+            for key, value in FileStorage.__objects.items():
+                if cls.__name__ == value.to_dict()['__class__']:
+                    new_objects[key] = value
+            return new_objects
+        else:
+            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +52,16 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ Delete an existing element in dictionary """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
+
+    def close(self):
+        """ Method for deserializing the JSON file to objects """
+        self.reload()
